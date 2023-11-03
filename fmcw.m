e44cd1cf42188726e21b43c21d347bbb0e3baa12
xXallisonXx <146761154+xXallisonXx@users.noreply.github.com>
2023-11-02 22:09:37 -0400

    Update fmcw.m
    may need to install wireless communications toolbox if you haven't already

--- fmcw.m
+++ fmcw.m
@@ -1,24 +1,44 @@
-%% S24-41 MDE FMCW simulation
-% created by allison 10/03/2023
+%% S24-41 MDE Transmit pulse simulation 
+% created by allison 11/02/2023
 % edited by: (^-^)
-clc, clear all
 
-fc = 5*10^3;                        % center freq
-BW = 1*10^3;                        % bandwidth
-n = 2*(fc+BW);                            % incrementing variable
-fRange = linspace(fc-BW, fc+BW, n); % range of pulse
-slope = 1;                          % slope of pulse in freq domain
-A = 1;                              % amplitude of pulse
-t = linspace(0, 0.01, n);        % time vector
+clc, clear
+% speed of light
+c = 3*10^8;
+% max unambiguous range
+Rmax = 10; % meters
+% time delay multiplier between pulses
+x = 100;
 
-pulse_t = zeros(1, n);
-for i = 1:n
-  pulse_t(i) = A*cos(fRange(i)*t(i));
-end
+%% Antenna info
+% patch antenna gain
+txGain = 9; % dBi
+% tx power
+txPwr = 1;  % Watt
 
-pulse_f = fft(pulse_t);
+%% Generating FMCW waveform
+% center frequency
+fc = 5*10^9;
+% pulse bandwidth (frequency)
+bw = 1*10^9;
+% samp rate
+fs = 22000000000; % 2*(fc + 0.5*bw), type mismatch error lol
+% sweep shape
+shape = 'Triangle';
+% pulsewidth (time)
+pw = 1*10^-7; % 2*Rmax/((x - 1)*c) ~1ns didn't work? too small?
+% only models one waveform for now
+sig = phased.FMCWWaveform('SampleRate', fs, ...
+    'SweepTime', pw, ...
+    'SweepBandwidth', bw, ...
+    'SweepDirection', 'Triangle', ...
+    'NumSweeps', 2);
+sig = step(sig);
+t = linspace(0, pw, length(sig));
 
-plot(t, pulse_t)
-xlabel('time (s)')
-ylabel('signal in time domain')
+plot(t, real(sig))
 
+% windowlength = 64;
+% noverlap = 32;
+% nfft = 64;
+% spectrogram(sig,windowlength,noverlap,nfft,fs,'yaxis')
